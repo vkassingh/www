0853ffe58eb97cad906c3c6766ecae4bcbf74b0a
Sasha Romijn <github@mxsasha.eu>
2020-09-14 12:34:28 +0200

    Add hopin monitor

--- docs/_scripts/hopin_monitor.py
+++ docs/_scripts/hopin_monitor.py
@@ -1,27 +1,87 @@
-
-import asyncio
-import websockets
+import sys
+import pysher
+import time
 import json
 
+import pickle
+import os.path
+import datetime
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
-async def hello():
-    uri = "wss://ws-eu.pusher.com/app/d9b03d485c6d62190e94?protocol=7&client=js&version=5.0.3&flash=false"
-    async with websockets.connect(uri) as websocket:
-        sub = '{"event":"pusher:subscribe","data":{"channel":"hopin-chat-event-SECRET"}}'
-        await websocket.send(sub)
-        print(f"> {sub}")
-        sub = '{"event":"pusher:subscribe","data":{"channel":"hopin-chat-stage-SECRET"}}'
-        await websocket.send(sub)
-        print(f"> {sub}")
+# Add a logging handler so we can see the raw communication data
+import logging
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+ch = logging.StreamHandler(sys.stdout)
+root.addHandler(ch)
 
-        while True:
-            ws_message = await websocket.recv()
-            message = json.loads(ws_message)
-            if message['event'] == 'pusher_internal:subscription_succeeded':
-                print('subscribe success')
-            elif message['event'] == 'message':
-                message_data = json.loads(message['data'])
-                print(f"{message['channel']}> {message_data['user']['name']}: {message_data['body']}")
+pusher = pysher.Pusher('SECRET', cluster='eu')
+service = None
 
-asyncio.get_event_loop().run_until_complete(hello())
 
+def message_handler(message_data):
+    global service
+    message = json.loads(message_data)
+    if message['body'].strip().startswith('Q:'):
+        question = message['body'].strip()[2:].strip()
+        print(f"Found question: {message['user']['name']} > {question}")
+        values = [[
+            datetime.datetime.now().strftime('%Y-%m-%d %H:%M'),
+            message['user']['name'],
+            question,
+        ]]
+        body = {
+            'values': values
+        }
+        service.spreadsheets().values().append(
+            spreadsheetId=DOCUMENT_ID, range='Sheet1',
+            valueInputOption='USER_ENTERED', body=body).execute()
+
+
+def connect_handler(data):
+    channel = pusher.subscribe('hopin-chat-stage-SECRET')
+    channel.bind('message', message_handler)
+
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# The ID of a sample document.
+DOCUMENT_ID = 'SECRET'
+
+
+def main():
+    global service
+    pusher.connection.bind('pusher:connection_established', connect_handler)
+    pusher.connect()
+
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    while True:
+        # Do other things in the meantime here...
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
